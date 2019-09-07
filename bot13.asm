5e9b385883120721ed89c9884ccbc31fd33b238a
Anisse Astier <anisse@astier.eu>
2019-09-07 13:14:19 +0200

    rewrite of bot11.asm to prevent radare2 ESIL bug
    Fixed upstream by pancake:
    https://github.com/radare/radare2/commit/7bcac956b181ac43960516dc31ca5234cd781df5

---
+++ bot13.asm
@@ -0,0 +1,16 @@
+# Anisse Astier
+#.arch arm
+#.bits 64
+adr x10, start
+mov x13, 1008
+sub x5, x5, 1
+mov x4, x5
+ldp x11, x12, [x10]
+stp x11, x12, [x13]
+stp x11, x12, [x13]
+br x13
+start:
+stp x5, x4, [x13, -16]!
+stp x5, x4, [x13, -16]!
+stp x5, x4, [x13, -16]!
+b start

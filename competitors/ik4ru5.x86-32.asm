958fd1c02c34b0a52301a4f906d3be60b0572f9b
Anisse Astier <anisse@astier.eu>
2019-09-07 13:19:33 +0200

    import competitors from last years to test against

---
+++ competitors/ik4ru5.x86-32.asm
@@ -0,0 +1,15 @@
+;Ik4ru5
+pop esp
+mov edi, 0x7600fc83
+mov esi, 0xcc8360fb 
+mov ebp, 0xfff5eb06
+mov eax, 0xffffffff
+mov ebx, 0xffffffff
+mov edx, 0xffffffff
+mov ecx, 0xffffffff
+mov esp, 0x400
+pushad
+mov edi, 0xffffffff
+mov esi, 0xffffffff
+mov eax, 0x3e0
+jmp eax
---
+++ competitors/t_pageflt.x86-32.asm
@@ -0,0 +1,12 @@
+; r2wars 2018
+; Dimitris Karagkasidis (@t_pageflt)
+mov eax, 0xc3c3c3c3
+mov ebx, eax
+mov ecx, eax
+mov edx, eax
+mov ebp, eax
+mov edi, eax
+mov esp, 1020
+mov esi, 1021
+mov [esi], 0xe6ff60
+jmp esi

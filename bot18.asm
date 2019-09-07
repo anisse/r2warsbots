81fdb5da45acf9e7cac3c96a9cd81f009884d410
Anisse Astier <anisse@astier.eu>
2019-09-07 13:20:50 +0200

    instead of nops, use stores so the cycles are more useful
    this delay helps beat ik4ru5

---
+++ bot18.asm
@@ -0,0 +1,28 @@
+# Anisse Astier
+#.arch arm
+#.bits 64
+adr x10, start
+mov x13, 1008
+mov x14, x13
+sub x5, x5, 1
+mov x4, x5
+ldp x11, x12, [x10]
+stp x11, x12, [x13]
+stp x11, x12, [x13]
+stp x11, x12, [x13]
+stp x11, x12, [x13]
+stp x11, x12, [x13]
+stp x11, x12, [x13]
+stp x11, x12, [x13]
+stp x11, x12, [x13]
+stp x11, x12, [x13]
+stp x11, x12, [x13]
+stp x11, x12, [x13]
+stp x11, x12, [x13]
+stp x11, x12, [x13]
+br x13
+start:
+stp x11, x12, [x13]
+stp x5, x4, [x14, -16]!
+stp x11, x12, [x13]
+b start
